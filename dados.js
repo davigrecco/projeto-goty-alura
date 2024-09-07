538e5378262203923b245d3fdf46851e795a9cb5
Davi Grecco <davigrecco.contato@gmail.com>
2024-09-07 00:36:04 -0300

    adicionando mais jogos

--- dados.js
+++ dados.js
@@ -9,7 +9,7 @@ let jogos = [
         titulo: "Like a Dragon: Infinite Wealth", 
         descricao: "Mergulhe no mundo de Yakuzas em Like a Dragon: Infinite Wealth. Com uma narrativa envolvente, combates emocionantes, este jogo promete uma experiência única.",
         link: "https://www.metacritic.com/game/like-a-dragon-infinite-wealth/",
-        tags: "yakuza ichiban kiryu havai yokohama"
+        tags: "yakuza ichiban kiryu havai yokohama mundo aberto"
     },
     {
         titulo: "Persona 3: Reload", 
@@ -22,6 +22,24 @@ let jogos = [
         descricao: "Retorne ao mundo mágico de Prince of Persia com um novo capítulo emocionante. Domine as artes marciais e manipule o tempo para superar desafios em um reino ancestral amaldiçoado.",
         link: "https://www.metacritic.com/game/prince-of-persia-the-lost-crown/",
         tags: "tempo persas sargon sete imortais"
+    },
+    {
+        titulo: "Final Fantasy VII Rebirth",
+        descricao: "Final Fantasy VII Rebirth é a continuação direta do aclamado remake de Final Fantasy VII, aprofundando a história épica que se passa no universo de Midgar. Após os eventos do primeiro jogo, que reimaginou a primeira parte do clássico original, Rebirth leva os jogadores em uma jornada épica pelo mundo de Gaia.",
+        link: "https://www.metacritic.com/game/final-fantasy-vii-rebirth/",
+        tags: "rpg remake aventura fantasia midgar square enix"
+    },
+    {
+        titulo: "Dragon's Dogma 2",
+        descricao: "Dragon's Dogma 2 é a aguardada sequência do RPG de ação que conquistou os fãs com sua jogabilidade única e mundo envolvente. Neste novo capítulo, os jogadores embarcam em uma aventura épica em um vasto mundo de fantasia, repleto de criaturas míticas, castelos imponentes e paisagens deslumbrantes.",
+        link: "https://www.metacritic.com/game/dragons-dogma-2/",
+        tags: "rpg capcom mundo aberto aventura"
+    },
+    {
+        titulo: "Astro Bot",
+        descricao: "Astro Bot é um jogo de plataforma 3D exclusivo para PlayStation 5 que conquistou os corações dos jogadores com sua jogabilidade inovadora, gráficos vibrantes e trilha sonora cativante. Lançado em 2024, o jogo acompanha as aventuras do pequeno robô Astro em uma jornada épica por diversos mundos temáticos, repletos de desafios e surpresas.",
+        link: "https://www.metacritic.com/game/astro-bot/",
+        tags: "ps5 playstation plataforma 3d infantil familia sony"
     }
     
     
